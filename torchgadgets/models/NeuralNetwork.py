badadd102219283c854c874503b19e3954508399
LDenninger <Luis0512@web.de>
2023-05-20 17:06:04 +0200

    Added recurrent models and the possibility to give a custom module to the model via the config

--- torchgadgets/models/NeuralNetwork.py
+++ torchgadgets/models/NeuralNetwork.py
@@ -7,47 +7,18 @@ class NeuralNetwork(nn.Module):
     
     def __init__(self, 
                     layers: list,):
-        """
-            Convolutional Neural Network. The layers are fully configurable by providing a simple config for each layer
 
-            Parameters:
-                layers (list): list of layers to be configured
-                    Format:
-                            [
-                                {
-                                    "type": "conv2d",
-                                    "in_channels": 1,
-                                    "out_channels": 1,
-                                    "kernel_size": (3,3),
-                                    "stride": (1,1),
-                                }, 
-                                {
-                                    "type": "relu"
-                                },
-                                {
-                                    "type": "maxpool2d",
-                                    "kernel_size": (2,2)
-                                },
-                                {
-                                    "type": "avgpool2d",
-                                    "kernel_size": (2,2)
-                                },
-                                {
-                                    "type": "linear",
-                                    "in_features": 512,
-                                    "out_features": 10
-                                }
-        """
 
         super(NeuralNetwork, self).__init__()
         self.backbone = None
 
         self.build_model(layers)
 
-
     def build_model(self, layer_config):
         layers = nn.ModuleList()
         for (i, layer) in enumerate(layer_config):
+
+            ##-- Feature Extractor Models --##
             if layer["type"] == "ResNet":
                 self.backbone = ResNet(size=layer['size'], layer=layer['remove_layer'], weights=layer['weights'])
                 layers.append(self.backbone)
@@ -61,27 +32,76 @@ class NeuralNetwork(nn.Module):
                 layers.append(self.backbone)
             elif layer["type"] == "ViT":
                 self.backbone = VisualTransformer(size=layer['size'], layer=layer['remove_layer'], weights=layer['weights'])
-            elif layer["type"] == "conv2d":
-                layers.append(nn.Conv2d(layer["in_channels"], layer["out_channels"], layer["kernel_size"], layer["stride"]))
-            elif layer["type"] == "relu":
-                layers.append(nn.ReLU())
-            elif layer["type"] == "softmax":
-                layers.append(nn.Softmax(dim=-1))
-            elif layer["type"] == "maxpool2d":
-                layers.append(nn.MaxPool2d(layer["kernel_size"], layer['stride']))
-            elif layer["type"] == "avgpool2d":
-                layers.append(nn.AvgPool2d(layer["kernel_size"], layer['stride']))
-            # Add a linear layer with a given activation function
+
+            ##-- MLP Layers --##
             elif layer['type'] == 'linear':
                 layers.append(nn.Linear(in_features=layer['in_features'], out_features=layer['out_features']))
-            elif layer['type'] =='batchnorm2d':
-                layers.append(nn.BatchNorm2d(layer['num_features'], layer['eps'], layer['momentum']))
-            elif layer['type'] =='batchnorm1d':
-                layers.append(nn.BatchNorm1d(layer['num_features'], layer['eps'], layer['momentum']))
             elif layer['type'] == 'dropout':
                 layers.append(nn.Dropout(layer['prob']))
             elif layer['type'] == 'flatten':
                 layers.append(nn.Flatten())
+            
+            ##-- CNN Layers --##
+            elif layer["type"] == "conv2d":
+                layers.append(nn.Conv2d(layer["in_channels"], layer["out_channels"], layer["kernel_size"], layer["stride"]))
+            elif layer["type"] == "maxpool2d":
+                layers.append(nn.MaxPool2d(layer["kernel_size"], layer['stride']))
+            elif layer["type"] == "avgpool2d":
+                layers.append(nn.AvgPool2d(layer["kernel_size"], layer['stride']))
+   
+
+            ##-- Recurrent Models --##
+            elif layer["type"] == "RNN":
+                layers.append(nn.RNN(input_size=layer["input_size"], 
+                                        hidden_size=layer["hidden_size"], 
+                                            dropout=layer['dropout'],
+                                                bidirectional=layer['bidirectional'],
+                                                    nonlinearity=layer['activation'], 
+                                                        num_layers=layer["num_layers"], 
+                                                            batch_first=True))
+            elif layer["type"] == "LSTM":
+                layers.append(nn.LSTM(input_size=layer["input_size"], 
+                                        hidden_size=layer["hidden_size"], 
+                                            dropout=layer['dropout'],
+                                                bidirectional=layer['bidirectional']))
+            elif layer["type"] == "GRU":
+                layers.append(nn.GRU(input_size=layer["input_size"], 
+                                        hidden_size=layer["hidden_size"], 
+                                            dropout=layer['dropout'],
+                                                bidirectional=layer['bidirectional']))
+            
+            ##-- Recurrent Cells --##
+            elif layer["type"] == "RNNCell":
+                layers.append(nn.RNNCell(input_size=layer["input_size"], 
+                                        hidden_size=layer["hidden_size"], 
+                                            dropout=layer['dropout'],
+                                                    nonlinearity=layer['activation']))
+            elif layer["type"] == "LSTMCell":
+                layers.append(nn.LSTMCell(input_size=layer["input_size"], 
+                                        hidden_size=layer["hidden_size"]))
+            elif layer["type"] == "GRUCell":
+                layers.append(nn.GRUCell(input_size=layer["input_size"], 
+                                        hidden_size=layer["hidden_size"]))
+            
+            ##-- Normalization Layers --##
+            elif layer['type'] =='batchnorm1d':
+                layers.append(nn.BatchNorm1d(layer['num_features'], layer['eps'], layer['momentum']))
+
+            elif layer['type'] =='batchnorm2d':
+                layers.append(nn.BatchNorm2d(layer['num_features'], layer['eps'], layer['momentum']))
+
+            ##-- Activation Functions --##
+            elif layer["type"] == "relu":
+                layers.append(nn.ReLU())
+            elif layer["type"] == "softmax":
+                layers.append(nn.Softmax(dim=-1))
+
+            ##-- Custom Module --##
+            elif layer["type"] == "custom":
+                layers.append(layer['module'])
+     
+
+
             
         self.model = nn.Sequential(*layers)
 
